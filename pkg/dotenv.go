706c85d3e40d6fe41c61bfa5fcd36ad047fd22dd
eyad-hussein <eyad.hussein@ejust.edu.eg>
2024-08-14 04:34:02 +0300

    feat: add support for parse, read, and load env files

---
+++ pkg/dotenv.go
@@ -0,0 +1,192 @@
+package pkg
+
+import (
+	"bufio"
+	"errors"
+	"fmt"
+	"io"
+	"log"
+	"os"
+	"regexp"
+	"strings"
+)
+
+var (
+	keyRegex           = regexp.MustCompile(`[a-zA-Z_]+[a-zA-Z0-9_]*`)
+	substituteVarRegex = regexp.MustCompile(`(\\)?(\$)(\()?\{?([A-Z0-9_]+)?\}?`)
+	escapeRegex        = regexp.MustCompile(`\\.`)
+	unescapeRegex      = regexp.MustCompile(`\\([^$])`)
+)
+
+// Read reads the environment variables from the given files and returns them as a map.
+func Read(filenames ...string) (map[string]string, error) {
+	envVars := make(map[string]string)
+
+	for _, filename := range filenames {
+		envFile, err := os.Open(filename)
+		if err != nil {
+			return nil, err
+		}
+		defer envFile.Close()
+
+		log.Println("reading file", filename)
+		vars, err := Parse(envFile)
+		if err != nil {
+			return nil, err
+		}
+
+		for key, val := range vars {
+			envVars[key] = val
+		}
+	}
+
+	return envVars, nil
+}
+
+// Parse parses the environment variables from the given file as a reader and returns them as a map.
+func Parse(envFile io.Reader) (map[string]string, error) {
+	envVars := make(map[string]string)
+
+	scanner := bufio.NewScanner(envFile)
+	for scanner.Scan() {
+		line := scanner.Text()
+
+		if strings.HasPrefix(line, "#") || len(strings.TrimSpace(line)) == 0 {
+			continue
+		}
+
+		line = strings.TrimPrefix(line, "export ")
+		keyVal := strings.SplitN(line, "=", 2)
+
+		if len(keyVal) != 2 {
+			return nil, errors.New("invalid line")
+		}
+
+		key := strings.TrimSpace(keyVal[0])
+		if matched := keyRegex.Match([]byte(line)); !matched {
+			return nil, fmt.Errorf("invalid key %s", key)
+		}
+		val, err := extractValue(keyVal[1], scanner, envVars)
+		if err != nil {
+			return nil, fmt.Errorf("error parsing value for key %s %w", key, err)
+		}
+
+		envVars[key] = val
+	}
+	return envVars, nil
+}
+
+// Load loads the environment variables from the given files into the current environment.
+func Load(filenames ...string) error {
+	envVars, err := Read(filenames...)
+	if err != nil {
+		return err
+	}
+
+	for key, val := range envVars {
+		if err := os.Setenv(key, val); err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
+
+func extractValue(val string, scanner *bufio.Scanner, currentEnvVars map[string]string) (string, error) {
+	if !strings.HasPrefix(val, "'") && !strings.HasPrefix(val, "\"") {
+		return parseEscape(substituteVariables(strings.TrimSpace(strings.Split(val, "#")[0]), currentEnvVars)), nil
+	}
+
+	var remaining string
+
+	if isMultiLine(val) {
+		var multilineVal strings.Builder
+
+		var prefix string
+		if strings.HasPrefix(val, "\"\"\"") {
+			prefix = "\"\"\""
+		} else {
+			prefix = "'''"
+		}
+		for scanner.Scan() {
+			line := scanner.Text()
+
+			if strings.HasSuffix(line, prefix) {
+				return strings.TrimSuffix(multilineVal.String(), "\n"), nil
+			}
+
+			line = strings.TrimSuffix(line, prefix)
+			line = strings.TrimSuffix(line, prefix)
+
+			if prefix == "\"\"\"" {
+				line = substituteVariables(line, currentEnvVars)
+			}
+			multilineVal.WriteString(parseEscape(line) + "\n")
+		}
+
+		return "", errors.New("unterminated multiline value")
+	} else {
+		var prefix byte
+
+		if strings.HasPrefix(val, "'") {
+			prefix = '\''
+		} else {
+			prefix = '"'
+		}
+		if strings.HasPrefix(val, string(prefix)) {
+			for i := 1; i < len(val); i++ {
+				if val[i] == prefix && !(i > 0 && val[i-1] == '\\') {
+					val, remaining = val[1:i], strings.TrimSpace(val[i+1:])
+				}
+			}
+
+			if strings.HasPrefix(val, string(prefix)) {
+				return "", errors.New("unterminated quoted value")
+			}
+		}
+
+		if prefix == '"' {
+			val = parseEscape(substituteVariables(val, currentEnvVars))
+		}
+	}
+
+	if strings.TrimSpace(remaining) != "" && !strings.HasPrefix(remaining, "#") {
+		return "", errors.New("unexpected characters after value")
+	}
+
+	return val, nil
+}
+
+func isMultiLine(line string) bool {
+	return strings.HasPrefix(line, "\"\"\"") || strings.HasPrefix(line, "'''")
+}
+
+func parseEscape(str string) string {
+	out := escapeRegex.ReplaceAllStringFunc(str, func(match string) string {
+		c := strings.TrimPrefix(match, `\`)
+		switch c {
+		case "n":
+			return "\n"
+		case "r":
+			return "\r"
+		case "t":
+			return "\t"
+		case "f":
+			return "\f"
+		case "b":
+			return "\b"
+		default:
+			return match
+		}
+	})
+	return unescapeRegex.ReplaceAllString(out, "$1")
+}
+
+func substituteVariables(line string, envVars map[string]string) string {
+	return substituteVarRegex.ReplaceAllStringFunc(line, func(match string) string {
+		if _, ok := envVars[match[2:len(match)-1]]; !ok {
+			return match
+		}
+		return envVars[match[2:len(match)-1]]
+	})
+}
